a83ccbd8f354b26f8e998819ca7f9a66d36e24c3
PiGGer <1455076726@qq.com>
2025-05-22 00:46:56 +0800

    level2 finish（maybe）

--- Assets/Scripts/MoveManager.cs
+++ Assets/Scripts/MoveManager.cs
@@ -15,6 +15,7 @@ public class MoveManager : MonoBehaviour
     public Vector3 CurrentWorldPosition = Vector3.zero;
     public Quaternion CurrentWorldRotation = Quaternion.identity;
     public Transform player;
+    private float verticalSpeed = 0.05f;
     private void Awake()
     {
         if (Instance == null)
@@ -50,17 +51,14 @@ public class MoveManager : MonoBehaviour
             localPosition = TrackingObject.localPosition;
             
         }
-     /*   if (Input.GetKeyDown(KeyCode.Space))
+     if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            *//* player.position = new Vector3(-TrackingObject.transform.localPosition.x *4f, player.position.y, -TrackingObject.transform.localPosition.z * 4f);
-             float yAngle = TrackingObject.transform.localEulerAngles.y;
-             player.rotation = Quaternion.Euler(0f, -yAngle, 0f);*//*
-            OnSceneIn();
+            transform.position += Vector3.up * verticalSpeed;
         }
-        if (Input.GetKeyDown(KeyCode.N))
+     if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            OnSceneOut();
-        }*/
+            transform.position += Vector3.down * verticalSpeed;
+        }
     }
     private bool TryGetDevicePosition(out Vector3 position)
     {
