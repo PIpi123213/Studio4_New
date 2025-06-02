9e9a6aae79268dba046387229072084fb33e28e8
Wu Junyi <3049118371@qq.com>
2025-06-02 19:25:33 +0800

    添加游泳教程

---
+++ Assets/Scripts/SwimTutorialDetectPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimTutorialDetectPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 检查碰撞的对象是否是玩家
+        if (other.CompareTag("Player"))
+        {
+            // 禁用当前游戏对象
+            gameObject.SetActive(false);
+        }
+    }
+}
